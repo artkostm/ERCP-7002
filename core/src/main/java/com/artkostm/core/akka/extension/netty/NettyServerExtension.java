b3eba234ce7a3c564f8cdf41d46c1bcd5c50d406
Artsiom Chuiko <v-achuiko@expedia.com>
2016-03-18 18:03:48 +0300

    Updated akka-netty extension

--- core/src/main/java/com/artkostm/core/akka/extension/netty/NettyServerExtension.java
+++ core/src/main/java/com/artkostm/core/akka/extension/netty/NettyServerExtension.java
@@ -1,5 +1,7 @@
 package com.artkostm.core.akka.extension.netty;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import com.artkostm.core.configuration.BeanFactory;
 import com.artkostm.core.configuration.internal.AppConfig;
 import com.typesafe.config.Config;
@@ -10,9 +12,20 @@ public class NettyServerExtension implements Extension
 {
     private final AppConfig appConfig;
     
+    private final AtomicBoolean isRunning;
+    
     public NettyServerExtension(final Config config)
     {
-        this.appConfig = BeanFactory.create(config.getConfig("app"), AppConfig.class);;
+        this.appConfig = BeanFactory.create(config.getConfig("app"), AppConfig.class);
+        isRunning = new AtomicBoolean(false);
+    }
+    
+    public void run()
+    {
+        if (!isRunning.get())
+        {
+            isRunning.compareAndSet(false, true);
+        }
     }
     
     public AppConfig config()
