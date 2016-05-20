54c75b2b1cdb883296e9941f3782e8b22f05e505
Artsiom Chuiko <v-achuiko@expedia.com>
2016-05-20 17:13:47 +0300

    fix configuration

--- core/src/test/java/com/artkostm/core/AkkaServer.java
+++ core/src/test/java/com/artkostm/core/AkkaServer.java
@@ -14,42 +14,42 @@ import akka.util.ByteString;
 
 public class AkkaServer extends HttpApp
 {
-    public static void main(String[] args) throws IOException 
-    {
-        // boot up server using the route as defined below
-        ActorSystem system = ActorSystem.create();
- 
-        new AkkaServer().bindRoute("127.0.0.1", 8060, system);
-        System.out.println("Type RETURN to exit");
-        System.in.read();
-        system.terminate();
-    }
-
-    //ab -n 20000 -c 10 http://localhost:8080/hello
+    //ab -n 20000 -c 10 http://localhost:8060/hello
     private RequestVal<String> name = Parameters.stringValue("name").withDefault("Mister X");
     
     @Override
     public Route createRoute()
     {
-        Route helloRoute =
-                handleWith1(name, (ctx, name) ->
+        Route helloRoute = handleWith1(name, 
+            (ctx, name) ->
                 { 
-                    CompletableFuture<ByteString> f = (CompletableFuture<ByteString>) ctx.request().entity().getDataBytes().fold(ByteString.empty(), (z, i) -> z.concat(i)).runWith(Sink.<ByteString>head(), ctx.materializer());
+                    final CompletableFuture<ByteString> f = (CompletableFuture<ByteString>) 
+                            ctx.request().entity().getDataBytes().fold(ByteString.empty(), 
+                                (z, i) -> z.concat(i)).runWith(Sink.<ByteString>head(), ctx.materializer());
                     return ctx.complete("Hello " + name + "!" + "\n" + f.get().utf8String()); 
                 });
         return route(
-                    post(pathSingleSlash().route(
-                            complete(ContentTypes.TEXT_HTML_UTF8,
-                                    "<html><body>Hello world!</body></html>")
-                        ),
-                        path("ping").route(
-                            complete("PONG!")
-                        ),
-                        path("hello").route(
-                            helloRoute
-                        )
+            post(pathSingleSlash().route(
+                complete(ContentTypes.TEXT_HTML_UTF8,"<html><body>Hello world!</body></html>")
+                ),
+                path("ping").route(
+                    complete("PONG!")
+                    ),
+                path("hello").route(
+                    helloRoute
                     )
-                );
+                )
+            );
+    }
+    
+    public static void main(String[] args) throws IOException 
+    {
+        final ActorSystem system = ActorSystem.create();
+ 
+        new AkkaServer().bindRoute("127.0.0.1", 8060, system);
+        System.out.println("Type RETURN to exit");
+        System.in.read();
+        system.terminate();
     }
 
 }
--- core/src/test/java/com/artkostm/core/akka/http/client/HttpRunnerTest.java
+++ core/src/test/java/com/artkostm/core/akka/http/client/HttpRunnerTest.java
@@ -1,8 +1,5 @@
 package com.artkostm.core.akka.http.client;
 
-import java.io.File;
-import java.io.FileWriter;
-
 import akka.actor.ActorSystem;
 import akka.stream.javadsl.Sink;
 import akka.util.ByteString;
@@ -17,43 +14,18 @@ public class HttpRunnerTest
     {
         final ActorSystem system = ActorSystem.create("http-client");
         final HttpEndpoint client = new HttpEndpoint(system);
-
-        final long start = System.currentTimeMillis();
-        final String sb1 = client.createFlow("www.bsuir.by", 80)
+        client.createFlow("www.bsuir.by", 80)
                 .request("/schedule/schedule.xhtml?id=272301").asString();
-        System.out.println(sb1);
         
-        for (int j = 0; j < 1; j++)
+        client.request("/schedule/schedule.xhtml?id=272301", (r, t) -> 
         {
-            client.request("/schedule/schedule.xhtml?id=272301", (r, t) -> 
-            {
-                r.entity().getDataBytes()
-                    .fold(ByteString.empty(), (z, i) -> z.concat(i))
-                    .runWith(Sink.<ByteString>head(), client.materializer())
-                    .whenCompleteAsync((s, e) -> sb.append(s.utf8String() + "\nTime: " + (System.currentTimeMillis() - start)), system.dispatcher());
-            });
-        }
+            r.entity().getDataBytes()
+                .fold(ByteString.empty(), (z, i) -> z.concat(i))
+                .runWith(Sink.<ByteString>head(), client.materializer())
+                .whenCompleteAsync((s, e) -> sb.append(s.utf8String()), system.dispatcher());
+        });
         
-        final String sb2 = client.createFlow("www.nurkiewicz.com", 80)
-                .request("/2013/05/java-8-definitive-guide-to.html").asString();
-        System.out.println(sb2);
-        
-        for (int j = 0; j < 1; j++)
-        {
-            client.request("/2013/05/java-8-definitive-guide-to.html", (r, t) -> 
-            {
-                r.entity().getDataBytes()
-                    .fold(ByteString.empty(), (z, i) -> z.concat(i))
-                    .runWith(Sink.<ByteString>head(), client.materializer())
-                    .whenCompleteAsync((s, e) -> sb.append(s.utf8String() + "\nTime: " + (System.currentTimeMillis() - start)), system.dispatcher());
-            });
-        }
-        System.out.println("Time: " + (System.currentTimeMillis() - start));
-        
-        Thread.sleep(20000);
-        FileWriter writer = new FileWriter(new File("http.requests.html"));
-        writer.write(sb.toString());
-        writer.flush();
-        writer.close();
+        Thread.sleep(3000);
+        System.out.println(sb);
     }
 }
