f6bdb8346e8097d10caa11ac0e144c2ef318b222
Artsiom Chuiko <v-achuiko@expedia.com>
2016-05-30 18:31:44 +0300

    updated config

--- core/src/test/java/example/controller/EchoController.java
+++ core/src/test/java/example/controller/EchoController.java
@@ -1,7 +1,17 @@
 package example.controller;
 
+import com.artkostm.core.akka.actors.ControllerActor;
+import com.artkostm.core.akka.http.message.HttpMessage;
+import com.artkostm.core.web.controller.Result;
+
 /**
  * Created by arttsiom.chuiko on 30/05/16.
  */
-public class EchoController {
+public class EchoController extends ControllerActor
+{
+    @Override
+    protected Result onRequest(HttpMessage msg) throws Exception
+    {
+        return ok(msg.payload().toString());
+    }
 }
--- core/src/test/java/example/second/SecondApp.java
+++ core/src/test/java/example/second/SecondApp.java
@@ -1,7 +1,15 @@
 package example.second;
 
+import com.artkostm.core.web.ApplicationBootstrap;
+import com.artkostm.core.web.WebApplicationAdapter;
+
 /**
  * Created by arttsiom.chuiko on 30/05/16.
  */
-public class SecondApp {
+public class SecondApp extends WebApplicationAdapter
+{
+    public static void main(String[] args)
+    {
+        ApplicationBootstrap.run(SecondApp.class, "/secondconfig.conf");
+    }
 }
