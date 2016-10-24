c22fb1fd75992b6003549e0a8edb829c23c3aa6f
Artsiom <artkostm@gmail.com>
2016-10-25 00:06:02 +0300

    added config example

--- ScalaTests/src/main/scala/com/artkostm/scala/test/AkkaHttpStart.scala
+++ ScalaTests/src/main/scala/com/artkostm/scala/test/AkkaHttpStart.scala
@@ -1,48 +1,48 @@
 package com.artkostm.scala.test
 
-import akka.actor.ActorSystem
-
-/*trait Core  {
-  implicit val system: ActorSystem = ActorSystem("Suggestion")
-  implicit val materializer: ActorFlowMaterializer = ActorFlowMaterializer()
-  implicit val ec: ExecutionContext = system.dispatcher
-}*/
+import com.typesafe.config.ConfigFactory
+import configs.Configs
+import configs.Result.Success
 
 object AkkaHttpStart extends App{
-  /*val http = new VanilaHttp with Core{
-    override def config: Config = ConfigFactory.load()
-  }
 
-  http.start() */
-  List("Sasha", "Masha", "Dasha", "Glasha").map { name => name.length() } foreach println
-}
+  //List("Sasha", "Masha", "Dasha", "Glasha").map { name => name.length() } foreach println
 
-trait Core{
-  def summarize() : String;
-}
-abstract class DefCore extends Core
-/*
-trait AkkaHttp {
-  this: Core =>
-
-  def config: Config
-
-  def start() = {
-    val route: Route = {
-      path("dictionaries" / Segment / "suggestions"){ dictionaryId =>
-        get{
-          parameters("ngr"){ ngr =>
-              complete("response")
-          }
-        }
+  val myConfig = ConfigFactory.parseString(
+    """
+      my-config {
+       foo = My config
+       bar = 1234
+       baz = [1h, 2m, 3s]
       }
-    }
 
-    Http().bind(
-      interface = config.getString("frontend.interface"),
-      port = config.getInt("frontend.port")
-    ).to(Sink.foreach { conn =>
-      conn.flow.join(route).run()
-    }).run()
-  }
-} */
+      routes {
+      	GET /path1 {
+      		class : example.controller.ApachePageController
+      		spin: 1
+      		name: first
+      	}
+
+          GET /path2 {
+      		class : example.controller.IndexController
+      		name: index
+      		spin: 5
+      	}
+
+      	POST /path1 {
+      		class : example.controller.NettyActor
+      		name: postactor
+      	}
+      }
+    """)
+  import configs.syntax._
+  val bars = myConfig.get[Any]("routes").fold(error => Vector.empty[Any], v => v.asInstanceOf[Vector[Any]])
+  bars.foreach(x => x match {
+    case Tuple2(path, data) => println(s"Path: $path, Data: $data")
+    case _ => println("Fail!")
+  })
+//  bars match {
+//    case Success(x) => println(x.getClass)
+//    case _ => println("Fail!")
+//  }
+}
