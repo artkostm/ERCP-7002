ddd293936e76714233e34ef4cbd301e99e2b426d
Artsiom <artkostm@gmail.com>
2017-01-09 20:04:28 +0300

    new iplementation

--- ScalaTests/src/main/scala/com/artkostm/configuration/Configuration.scala
+++ ScalaTests/src/main/scala/com/artkostm/configuration/Configuration.scala
@@ -1,7 +1,10 @@
 package com.artkostm.configuration
 
-import akka.actor.{ActorSystem, ExtendedActorSystem, Extension, ExtensionId, ExtensionIdProvider}
+import akka.actor.{Actor, ActorSystem, ExtendedActorSystem, Extension, ExtensionId, ExtensionIdProvider}
 import com.typesafe.config.{Config, ConfigObject}
+
+import scala.reflect.ClassTag
+import scala.util.Try
 
 /**
   * Created by artsiom.chuiko on 24/10/2016.
@@ -23,8 +26,24 @@ object Configuration extends ExtensionId[ConfigExtensionImpl] with ExtensionIdPr
 
     def getOptInt(path: String) : Option[Int] = if (underlying.hasPath(path)) Some(underlying.getInt(path)) else None
   }
-}
 
+  implicit class RichConfigObject(val underlying: ConfigObject) extends AnyVal {
+    def getClazz(path: String) : Class[_ <: Actor] =  getClassFor[Actor](underlying.get(path).render()).get
+    def getInt(path: String, default: Option[Int] = None) =
+      if (underlying.containsKey(path)) Some(underlying.get(path).render().toInt)
+      else default
+    def getString(path: String, default: Option[String] = None) =
+      if (underlying.containsKey(path)) Some(underlying.get(path).render())
+      else default
+
+    def getClassFor[T: ClassTag](fqcn: String): Try[Class[_ <: T]] =
+      Try[Class[_ <: T]]({
+        val c = Class.forName(fqcn, false, getClass.getClassLoader).asInstanceOf[Class[_ <: T]]
+        val t = implicitly[ClassTag[T]].runtimeClass
+        if (t.isAssignableFrom(c)) c else throw new ClassCastException(t + " is not assignable from " + c)
+      })
+  }
+}
 
 class ConfigExtensionImpl(val config: Config) extends Extension {
 
@@ -33,20 +52,22 @@ class ConfigExtensionImpl(val config: Config) extends Extension {
   val netty = Netty(config.getOptString("app.netty.host"), config.getOptInt("app.netty.port"))
 
   def createRoutes()(implicit system: ActorSystem): Unit = {
-
+    println(template)
+    println(netty)
+    println(readRoutes())
   }
 
-  private def readRoutes(): Unit = {
+  private def readRoutes(): Vector[(String, RouteHolder)] = {
     import configs.syntax._
-    val routes = config.get[Any]("routes").fold(error => Vector.empty[Any], v => v.asInstanceOf[Vector[Any]])
-    routes.foreach(x => x match {
-      case Tuple2(path, data) => println(s"Path: $path, Data: ${data.asInstanceOf[ConfigObject]}")
-      case _ => println("Fail!")
+    import Configuration.RichConfigObject
+    val routes = config.get[Any]("routes").map(a => a.asInstanceOf[Vector[(String, ConfigObject)]])
+    routes.value.map(tuple => tuple match {
+      case (s, c) => (s, RouteHolder(c.getClazz("class"), c.getString("name"), c.getInt("spin", Some(1))))
     })
   }
 }
 
 case class Template(directory: Option[String])
 case class Netty(host: Option[String], port: Option[Int])
-case class RouteHolder(clazz: Option[String], name: Option[String], spin: Option[Int] = Some(1))
+case class RouteHolder(clazz: Class[_ <:Actor], name: Option[String], spin: Option[Int] = Some(1))
 
